41017170de841fc4b62b88519008ff73ff80b27f
Joël Troch <joel.troch62@gmail.com>
2017-01-06 15:04:09 +0100

    Bootstrap the project with Maven

---
+++ src/main/java/fr/lerangdufond/serpentin/Main.java
@@ -0,0 +1,18 @@
+package fr.lerangdufond.serpentin;
+
+/**
+ * Main class for the Serpentin.
+ *
+ * @author Joel Troch
+ */
+public class Main {
+
+	/**
+	 * Entry point of the application.
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/fr/lerangdufond/serpentin/MainTest.java
@@ -0,0 +1,10 @@
+package fr.lerangdufond.serpentin;
+
+/**
+ * Test class for the "Main" class.
+ *
+ * @author Joel Troch
+ */
+public class MainTest {
+
+}
