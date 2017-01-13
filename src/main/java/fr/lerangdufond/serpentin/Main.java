093cba430c8f8a60ac8f050c16fcc4fbb90237a4
Robin Bossart <robin.bossart@univ-lille1.fr>
2017-01-13 14:27:03 +0100

    Modification of the main class to launch the serpentin game.

--- src/main/java/fr/lerangdufond/serpentin/Main.java
+++ src/main/java/fr/lerangdufond/serpentin/Main.java
@@ -1,29 +1,20 @@
 package fr.lerangdufond.serpentin;
 
-import gameframework.drawing.GameCanvas;
-import gameframework.drawing.GameCanvasDefaultImpl;
-import gameframework.game.GameConfiguration;
-import gameframework.game.GameData;
-import gameframework.gui.GameWindow;
+import gameframework.game.Game;
 
 /**
  * Main class for the Serpentin.
  *
- * @author Joel Troch
+ * @author Joel Troch - Robin Bossart - Francois Masson
  */
 public class Main {
-
 	/**
 	 * Entry point of the application.
 	 * @param args Command line arguments.
 	 */
 	public static void main(String[] args) {
-		GameConfiguration gc = new GameConfiguration();
-		String gameName = "Serpentin";
-		GameCanvas gameCanvas = new GameCanvasDefaultImpl();
-		GameData data = new GameData(gc);
-		GameWindow window = new GameWindow(gameName, gameCanvas, data);
-		window.createGUI();
+		Game serpentin = new Serpentin();
+		serpentin.start();
 	}
 
 }
