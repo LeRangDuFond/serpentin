db11c597e2f86f023905be3ed8eb11a0a68f16c5
Joël Troch <joel.troch62@gmail.com>
2017-01-27 15:18:32 +0100

    Remove "Life" from the status bar

--- src/main/java/fr/lerangdufond/serpentin/Main.java
+++ src/main/java/fr/lerangdufond/serpentin/Main.java
@@ -4,6 +4,7 @@ import fr.lerangdufond.serpentin.game.SerpentinConfiguration;
 import fr.lerangdufond.serpentin.game.SerpentinLevel;
 import gameframework.game.GameData;
 import gameframework.game.GameDefaultImpl;
+import gameframework.gui.GameStatusBarElement;
 import gameframework.gui.GameWindow;
 
 /**
@@ -17,11 +18,17 @@ public class Main {
 	 * @param args Command line arguments.
 	 */
 	public static void main(String[] args) {
+		// Define the assets to use
 		SerpentinConfiguration gameConfig = new SerpentinConfiguration();
 		GameData gameData = new GameData(gameConfig);
 		gameData.addLevel(new SerpentinLevel(gameData));
 		GameDefaultImpl game = new GameDefaultImpl(gameData);
-		GameWindow window = new GameWindow("Serpentin", gameData.getCanvas(), gameData);
+
+		// Status bar
+		GameStatusBarElement<Integer> gameScoreBar = new GameStatusBarElement<>("Score : ", gameData.getScore());
+
+		// Create the window and start the game
+		GameWindow window = new GameWindow("Serpentin", gameData.getCanvas(), gameConfig, gameScoreBar);
 		window.createGUI();
 		game.start();
 	}
