317e0214166cc948d137392e346fbf09a8970982
Robin Bossart <bssrobin@gmail.com>
2017-01-27 14:51:35 +0100

    Classes creation for create new movment : turn back is not allowed in serpentin game

---
+++ src/main/java/fr/lerangdufond/serpentin/game/utils/SerpentinMoveStrategyConfigurableKeyboard.java
@@ -0,0 +1,51 @@
+package fr.lerangdufond.serpentin.game.utils;
+
+import java.awt.Point;
+
+import gameframework.motion.MoveStrategyConfigurableKeyboard;
+import gameframework.motion.SpeedVector;
+
+public class SerpentinMoveStrategyConfigurableKeyboard extends MoveStrategyConfigurableKeyboard{
+	
+	private Point prevDirection;
+	
+	SerpentinMoveStrategyConfigurableKeyboard(Boolean alwaysMove, SpeedVector speedVector, Boolean combineDirections){
+		super(alwaysMove, speedVector, combineDirections);
+		this.prevDirection = new Point(0,0);
+	}
+	
+	/**
+	 * Update the direction depending on the keys pressed
+	 */
+	protected void updateDirection() {
+		final Point newDirection = new Point(0, 0);
+		for (Integer keyCode : keyPressed) {
+			final Point keyDirection = directions.get(keyCode);
+			if (keyDirection != null) {
+				System.out.println("x"+keyDirection.x+"y"+keyDirection.y);
+				/*Check if player is trying to return back.*/
+				/*Check if it is not the first move.*/
+				if((this.prevDirection.x + this.prevDirection.y) != 0){
+					if((keyDirection.x == 0 && this.prevDirection.x == 0) || (keyDirection.y == 0 && this.prevDirection.y == 0)){
+						return;
+					}
+				}
+				newDirection.x += keyDirection.x;
+				newDirection.y += keyDirection.y;
+				this.prevDirection.x = newDirection.x;
+				this.prevDirection.y = newDirection.y;
+				
+				// If we don't combine directions, then we should stop here
+				if (!combineDirections) {
+					break;
+				}
+			}
+		}
+		
+		if (newDirection.x != 0 || newDirection.y != 0 || !alwaysMove)
+			speedVector.setDirection(newDirection);
+	}
+	
+}
+
+
---
+++ src/main/java/fr/lerangdufond/serpentin/game/utils/SerpentinMoveStrategyKeyboard.java
@@ -0,0 +1,50 @@
+package fr.lerangdufond.serpentin.game.utils;
+
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+
+import gameframework.motion.SpeedVector;
+
+/**
+ * {@link MoveStrategy} which listens to the keyboard and answers new
+ * {@link SpeedVector speed vectors} based on what the user typed.
+ */
+public class SerpentinMoveStrategyKeyboard extends SerpentinMoveStrategyConfigurableKeyboard {
+	public SerpentinMoveStrategyKeyboard() {
+		this(true);
+	}
+	
+	/**
+	 * {@link MoveStrategyConfigurableKeyboard#MoveStrategyKeyboard(Boolean)}
+	 */
+	public SerpentinMoveStrategyKeyboard(Boolean alwaysMove) {
+		this(alwaysMove, new SpeedVector(new Point(0, 0)));
+	}
+	
+	/**
+	 * {@link MoveStrategyConfigurableKeyboard#MoveStrategyKeyboard(SpeedVector)}
+	 */
+	public SerpentinMoveStrategyKeyboard(SpeedVector speedVector) {
+		this(true, speedVector);
+	}
+	
+	/**
+	 * {@link MoveStrategyConfigurableKeyboard#MoveStrategyKeyboard(Boolean, SpeedVector)}
+	 */
+	public SerpentinMoveStrategyKeyboard(Boolean alwaysMove, SpeedVector speedVector) {
+		this(alwaysMove, speedVector, false);
+	}
+	
+	/**
+	 * {@link MoveStrategyConfigurableKeyboard#MoveStrategyKeyboard(Boolean, SpeedVector, Boolean)}
+	 */
+	public SerpentinMoveStrategyKeyboard(Boolean alwaysMove, SpeedVector speedVector, Boolean combineDirections) {
+		super(alwaysMove, speedVector, combineDirections);
+		
+		addKeyDirection(KeyEvent.VK_RIGHT, new Point(1, 0));
+		addKeyDirection(KeyEvent.VK_LEFT, new Point(-1, 0));
+		addKeyDirection(KeyEvent.VK_DOWN, new Point(0, 1));
+		addKeyDirection(KeyEvent.VK_UP, new Point(0, -1));
+	}
+}
+
