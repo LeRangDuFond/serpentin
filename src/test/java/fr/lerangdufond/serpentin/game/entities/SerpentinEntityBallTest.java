074c7365cdbf27c4b8638f5cfb0d531bf2b7678b
Joël Troch <joel.troch62@gmail.com>
2017-02-03 15:07:35 +0100

    Fix errors with unit tests

--- src/test/java/fr/lerangdufond/serpentin/game/entities/SerpentinEntityBallTest.java
+++ src/test/java/fr/lerangdufond/serpentin/game/entities/SerpentinEntityBallTest.java
@@ -10,15 +10,13 @@ import fr.lerangdufond.serpentin.game.SerpentinConfiguration;
 import gameframework.game.GameData;
 
 public class SerpentinEntityBallTest {
-
 	@Test
 	public void testGetBoundingBox() {
 		SerpentinConfiguration gameConfig = new SerpentinConfiguration();
 		GameData gameData = new GameData(gameConfig);
 		SerpentinEntityBall seb = new SerpentinEntityBall(gameData);
-		Rectangle rectangle = new Rectangle(seb.spriteSize, seb.spriteSize);
-		rectangle.setLocation(seb.getPosition().x * seb.spriteSize, seb.getPosition().y * seb.spriteSize);
+		Rectangle rectangle = new Rectangle(gameConfig.getSpriteSize(), gameConfig.getSpriteSize());
+		rectangle.setLocation(seb.getPosition().x * gameConfig.getSpriteSize(), seb.getPosition().y * gameConfig.getSpriteSize());
 		assertTrue(seb.getBoundingBox().equals(rectangle));
 	}
-
 }
--- src/test/java/fr/lerangdufond/serpentin/game/entities/SerpentinEntityPlayerTest.java
+++ src/test/java/fr/lerangdufond/serpentin/game/entities/SerpentinEntityPlayerTest.java
@@ -10,21 +10,19 @@ import fr.lerangdufond.serpentin.game.SerpentinConfiguration;
 import gameframework.game.GameData;
 
 public class SerpentinEntityPlayerTest {
-
 	@Test
 	public void testSerpentinEntityPlayer() {
 		SerpentinConfiguration gameConfig = new SerpentinConfiguration();
 		GameData gameData = new GameData(gameConfig);
 		SerpentinEntityPlayer sep = new SerpentinEntityPlayer(gameData);
-		assertTrue(sep.getBoundingBox().equals(new Rectangle(32,32)));
+		assertTrue(sep.getBoundingBox().equals(new Rectangle(gameConfig.getSpriteSize(), gameConfig.getSpriteSize())));
 	}
-	
+
 	@Test
 	public void testGetBoundingBox() {
 		SerpentinConfiguration gameConfig = new SerpentinConfiguration();
 		GameData gameData = new GameData(gameConfig);
 		SerpentinEntityPlayer sep = new SerpentinEntityPlayer(gameData);
-		assertTrue(sep.getBoundingBox().equals(new Rectangle(32,32)));
+		assertTrue(sep.getBoundingBox().equals(new Rectangle(gameConfig.getSpriteSize(), gameConfig.getSpriteSize())));
 	}
-	
 }
